65c06e489fded4682f7e53f0831e185e71c9d5aa
Najeeb Arif <NajeebArif786@outlook.com>
2021-09-02 01:43:22 +0530

    Update chapter2.ts
    Add code for type inference

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -1,4 +1,6 @@
 
+console.log('-------------------------------------------------------------------------------------------')
+
 // to use Symbol as primitive, 
 // es2015 needs to be added to the 'lib' key of compiler options in tsconfig.json
 const ord = Symbol('orderId')
@@ -9,3 +11,18 @@ const myOrder = {
 
 // DOM also needs to be included in 'lib' to use console.log
 console.log(myOrder['ord'])
+
+console.log('-------------------------------------------------------------------------------------------')
+
+//Type inference in typescript
+const age = 25;
+
+function getTax(income: number) : number {
+    return income * 0.15;
+}
+
+let yourTax = getTax(5000);
+
+console.log(yourTax);
+
+console.log('-------------------------------------------------------------------------------------------')
