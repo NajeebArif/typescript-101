0cc4bacab05ef62220a53ca3918be481f6c4919b
Najeeb Arif <NajeebArif786@outlook.com>
2021-09-04 00:07:44 +0530

    Update index.ts
    fix the error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 import chalk from "chalk";
-const bgGreen(chalk.black.bgGreenBright)
+const bgGreen = chalk.black.bgGreenBright
 const message: string = "Bundled with Webpack"
 console.log(bgGreen('Hello World'))
 console.log(bgGreen(message))
