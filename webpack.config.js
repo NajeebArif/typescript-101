104e5ae3863e4e14363e4c1e5345838901172ca0
Najeeb Arif <NajeebArif786@outlook.com>
2021-09-04 00:07:35 +0530

    Update webpack.config.js
    Add loader to preprocess ts files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const {resolve} = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.ts',
     output: {
         filename: 'index.bundle.js',
         path: resolve(__dirname,'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts','.js']
     },
     target: 'node',
     mode: 'development',
